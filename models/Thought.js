50d6466bd8bba108bbbcba5b9ec8c6a0009873cf
Cesar's PC <cesar261110@gmaill.com>
2022-09-21 15:13:15 -0700

    model for thoughts started

---
+++ models/Thought.js
@@ -0,0 +1,12 @@
+// Defining Schema, Types
+const { Schema, Types } = require('mongoose')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+    }
+)
