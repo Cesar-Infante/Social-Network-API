f0687a2e50c7aea3f6de882b192edf245f0b6155
Cesar's PC <cesar261110@gmaill.com>
2022-09-21 20:47:27 -0700

    reaction.js and thought.js model testing

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,7 @@
 // Defining Schema, Types
 const { Schema, Types } = require('mongoose')
 
+// schema to create Thought model
 const thoughtSchema = new Schema(
     {
         thoughtText: {
@@ -8,5 +9,38 @@ const thoughtSchema = new Schema(
             required: true,
             maxLength: 280
         },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => timeSince(date),
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: {
+            type: Schema.Types.ObjectId,
+            ref: 'reaction'
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
+
+// Virtual that retrieves the length of the thoughts reactions
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return `${this.reactions.length}`
+    });
+
+
+// Initialize our Thought model
+const Thought = model('thought', thoughtSchema);
+
+// exporting the model Thought 
+module.exports = Thought;
